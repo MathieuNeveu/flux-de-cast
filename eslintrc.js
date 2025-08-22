26097eaa89e285807aba6d3f19bf0e6c2d5734b7
Mathieu Neveu <m@mathn.fr>
2025-08-22 22:35:14 +0200

    Create eslintrc.js

---
+++ eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/essential' // Si tu utilises Vue (optionnel)
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn'
+  }
+};
